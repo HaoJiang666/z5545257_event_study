348713a43dfe6f8c69eb4aa179e2fee8ba7d9da7
HaoJiang666 <1059166014@qq.com>
2024-07-16 14:41:09 +1000

    Add files via upload

---
+++ toolkit_config.py
@@ -0,0 +1,11 @@
+# *** toolkit_config.py
+# Project configuration file
+# ***
+
+import os
+# 导入python内置的os模块(os.py这个代码文件)
+
+PRJDIR = r'C:\\Users\\姜灏\\toolkit\\toolkit'
+DATADIR = os.path.join(PRJDIR, 'data')
+# 通过'.'就可以使用os模块内部的全部功能
+
